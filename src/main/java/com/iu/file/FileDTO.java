cea394a896e23d242734869263b2223acc22990a
bomblike611 <rhtn611@naver.com>
2018-01-18 21:27:34 +0900

    ee
    ee

---
+++ src/main/java/com/iu/file/FileDTO.java
@@ -0,0 +1,28 @@
+package com.iu.file;
+
+public class FileDTO {
+
+	private int num;
+	private String Oname;
+	private String Fname;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getOname() {
+		return Oname;
+	}
+	public void setOname(String oname) {
+		Oname = oname;
+	}
+	public String getFname() {
+		return Fname;
+	}
+	public void setFname(String fname) {
+		Fname = fname;
+	}
+	
+	
+}
